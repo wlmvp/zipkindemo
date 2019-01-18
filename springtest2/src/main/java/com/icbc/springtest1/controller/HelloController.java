f28141be7bb4def0846e90eaa4ce3c0b822f61c6
wlmvp <wlmvp@sina.com>
2019-01-18 18:54:37 +0800

    升级brave版本，增加日志打印携带tranceid

--- springtest2/src/main/java/com/icbc/springtest1/controller/HelloController.java
+++ springtest2/src/main/java/com/icbc/springtest1/controller/HelloController.java
@@ -1,15 +1,17 @@
 package com.icbc.springtest1.controller;
 
 import com.github.kristofa.brave.Brave;
-import com.github.kristofa.brave.ServerTracer;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -18,10 +20,12 @@ import java.util.concurrent.TimeUnit;
 @EnableAutoConfiguration
 public class HelloController {
 
+    private final static Logger LOG = LoggerFactory.getLogger(HelloController.class);
+
+    //    @Autowired
+//    private CloseableHttpClient httpClient;
     @Autowired
-    private CloseableHttpClient httpClient;
-    @Autowired
-    private Brave brave;
+    RestTemplate restTemplate;
 
     private Random random = new Random();
 
@@ -31,14 +35,20 @@ public class HelloController {
 
         int sleep = random.nextInt(1000);
         TimeUnit.MILLISECONDS.sleep(sleep);
-        HttpGet get = new HttpGet("http://localhost:8083/service3");
-        CloseableHttpResponse response = httpClient.execute(get);
-        String result = EntityUtils.toString(response.getEntity(), "utf-8");
-        get = new HttpGet("http://localhost:8084/service4");
-        response = httpClient.execute(get);
-        result += EntityUtils.toString(response.getEntity(), "utf-8");
+//        HttpGet get = new HttpGet("http://localhost:8083/service3");
+//        LOG.info("get http://localhost:8082/service3,sleep:{}",sleep);
+//        CloseableHttpResponse response = httpClient.execute(get);
+//        String result = EntityUtils.toString(response.getEntity(), "utf-8");
+//        get = new HttpGet("http://localhost:8084/service4");
+//        response = httpClient.execute(get);
+//        result += EntityUtils.toString(response.getEntity(), "utf-8");
+//        return "[service2]:" + result;
 
-        ServerTracer trace = brave.serverTracer();
-        return "[service2]:" + result;
+
+        LOG.info("get http://localhost:8083/service3,sleep:{}",sleep);
+        String data1 = restTemplate.getForObject("http://localhost:8083/service3", String.class);
+        LOG.info("get http://localhost:8084/service3,sleep:{}",sleep);
+        String data2 = restTemplate.getForObject("http://localhost:8084/service4", String.class);
+        return "[service1]:" + data1 + data2;
     }
 }
